39a5f1a404e3bcfc83056880ecf70aa4ddf9dd01
Shanil <shanil@citrusinformatics.com>
2020-11-03 20:05:01 +0530

    Updates

---
+++ precache-manifest.0fc0ab04216cbe96f6a37e49f6e6678b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243d8f4e1f8353a5ff22c26816d0132f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da096c35d4f09dabb299",
+    "url": "./static/css/2.cf556e4b.chunk.css"
+  },
+  {
+    "revision": "a9878d6cc18701dcc3bf",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "da096c35d4f09dabb299",
+    "url": "./static/js/2.7094a30d.chunk.js"
+  },
+  {
+    "revision": "bf91be5e9948b1f35d07a0db4100fece",
+    "url": "./static/js/2.7094a30d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9878d6cc18701dcc3bf",
+    "url": "./static/js/main.4b3109cf.chunk.js"
+  },
+  {
+    "revision": "e98cf4224af2b9c1d608",
+    "url": "./static/js/runtime-main.6f2665d6.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
